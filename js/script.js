4413a582aa07cd2c05bff3d84a9fe74c411b0b31
Osama6622 <osamanoshey71@gmail.com>
2021-09-02 15:23:35 +0200

    Upload all files of punchbag game

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+  el: "#game",
+  data: {
+    health: 100,
+    ended: false,
+  },
+  methods: {
+    punch: function () {
+      this.health -= 10;
+      if (this.health <= 0) {
+        this.ended = true;
+      }
+    },
+    restart: function () {
+      this.health = 100;
+      this.ended = false;
+    },
+  },
+  computed: {},
+});
